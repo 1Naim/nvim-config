62ac459859d9f24958bcb12e762f1475246d5fcb
Eric Naim <dnaim@cachyos.org>
2025-03-23 02:45:39 +0800

    misc: Fix indents
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/naim/misc.lua
+++ lua/naim/misc.lua
@@ -2,21 +2,21 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Delete trailing whitespaces on write
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-  pattern = { '*' },
-  callback = function()
-    local save_cursor = vim.fn.getpos '.'
-    pcall(function()
-      vim.cmd [[%s/\s\+$//e]]
-    end)
-    vim.fn.setpos('.', save_cursor)
-  end,
+    pattern = { '*' },
+    callback = function()
+        local save_cursor = vim.fn.getpos '.'
+        pcall(function()
+            vim.cmd [[%s/\s\+$//e]]
+        end)
+        vim.fn.setpos('.', save_cursor)
+    end,
 })
