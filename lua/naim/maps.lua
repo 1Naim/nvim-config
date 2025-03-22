9b1f0ba12de51a8146e7791cd3876069e642e37e
Eric Naim <dnaim@cachyos.org>
2025-03-23 02:29:54 +0800

    Add more keymaps
    ala primeagen
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/naim/maps.lua
+++ lua/naim/maps.lua
@@ -1,2 +1,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- greatest remap ever -- le primeagen
+vim.keymap.set("x", "p", [["_dP]])
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
