7907e5faa5b37907dc446f8e878eb1a66284da79
Eric Naim <dnaim@cachyos.org>
2025-03-23 14:00:25 +0800

    plugins: Add vim-fugitive
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

---
+++ lua/naim/plugins/fugitive.lua
@@ -0,0 +1,36 @@
+return {
+    'tpope/vim-fugitive',
+    config = function()
+        vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+        local naim_fugitive = vim.api.nvim_create_augroup("naim_fugitive", {})
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = naim_fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = {buffer = bufnr, remap = false}
+                vim.keymap.set("n", "<leader>gp", function()
+                    vim.cmd.Git('pull')
+                end, opts)
+
+                vim.keymap.set("n", "<leader>gP", function()
+                    vim.cmd.Git({'push'})
+                end, opts)
+
+                vim.keymap.set("n", "<leader>ga", function()
+                    vim.cmd.Git({'add', '-u'})
+                end, opts)
+
+                vim.keymap.set("n", "<leader>gc", function()
+                    vim.cmd.Git({'commit', '-s'})
+                end, opts)
+            end,
+        })
+    end
+}
