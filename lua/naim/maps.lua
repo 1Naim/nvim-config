e003a371fda904919b39fa26db3722908645cc44
Eric Naim <dnaim@cachyos.org>
2025-03-23 17:18:01 +0800

    maps: Add keymap to create a new tab
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/naim/maps.lua
+++ lua/naim/maps.lua
@@ -15,3 +15,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Tab navigation
+vim.keymap.set("n", "<C-t>", "<cmd>tabnew<CR>")
+
