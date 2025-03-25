bef05ca8a2e5148192fb9f442fd3d9842cbcf0d3
Eric Naim <dnaim@cachyos.org>
2025-03-25 21:56:55 +0800

    plugins: autocomplete: Use blink.cmp instead of nvim-cmp
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

---
+++ lua/naim/plugins/autocomplete.lua
@@ -0,0 +1,16 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = 'v0.14.2',
+    opts = {
+        keymap = {
+            preset = 'enter',
+
+            ['<S-TAB>'] = { 'select_prev', 'fallback' },
+            ['<TAB>'] = { 'select_next', 'fallback' },
+    },
+        appearance = {
+            nerd_font_variant = 'mono',
+        },
+    },
+}
