683aae2d8d8d4d13dff545aff467998bc90657f5
Eric Naim <dnaim@cachyos.org>
2025-03-25 21:57:54 +0800

    plugins: Use standalone mini.statusline
    Others are not used.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

---
+++ lua/naim/plugins/statusline.lua
@@ -0,0 +1,14 @@
+return {
+    'echasnovski/mini.statusline',
+    version = '*',
+    config = function()
+        local statusline = require 'mini.statusline'
+
+        statusline.setup { use_icons = true }
+
+        require('mini.statusline').section_location = function()
+            return '%2l:%-2v'
+        end
+
+    end
+}
