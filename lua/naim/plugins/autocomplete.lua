ec9df312b65bc3b3135ee1bb519897a8a275780e
Eric Naim <dnaim@cachyos.org>
2025-03-29 17:26:36 +0800

    plugins: Sync lsp config
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/naim/plugins/autocomplete.lua
+++ lua/naim/plugins/autocomplete.lua
@@ -1,12 +1,23 @@
 return {
     'saghen/blink.cmp',
     version = '1.*',
+    event = 'VimEnter',
     opts = {
         keymap = {
-            preset = 'enter',
+            preset = 'none',
 
-            ['<S-TAB>'] = { 'select_prev', 'fallback' },
-            ['<TAB>'] = { 'select_next', 'fallback' },
+            ['<S-TAB>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<TAB>'] = { 'select_next', 'fallback_to_mappings' },
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide' },
+            ['<CR>'] = { 'select_and_accept', 'fallback' },
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+            ['<C-n>'] = { 'snippet_forward', 'fallback' },
+            ['<C-p>'] = { 'snippet_backward', 'fallback' },
+            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
         },
         appearance = {
             nerd_font_variant = 'mono',
