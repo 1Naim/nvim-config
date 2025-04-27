6fc99b0ad475c5d7cd25172bb223b3b6be2a3e7f
Eric Naim <dnaim@cachyos.org>
2025-04-28 00:13:51 +0800

    gitsigns: Remove useless function
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,26 +1,3 @@
-function git_my_lines()
-    -- Properly link GitSigns highlights to default Git colors
-    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'DiffAdd' })
-    vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'DiffAdd' })
-    vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'DiffAdd' })
-
-    vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'DiffChange' })
-    vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'DiffChange' })
-    vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'DiffChange' })
-
-    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'DiffDelete' })
-    vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'DiffDelete' })
-    vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'DiffDelete' })
-
-    vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'DiffDelete' })
-    vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { link = 'DiffDelete' })
-    vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'DiffDelete' })
-
-    vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'DiffChange' })
-    vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { link = 'DiffChange' })
-    vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'DiffChange' })
-end
-
 return {
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -31,8 +8,5 @@ return {
             topdelete = { text = '‾' },
             changedelete = { text = '~' },
         },
-        on_attach = function()
-            git_my_lines() -- Run only after gitsigns loads
-        end,
     },
 }
