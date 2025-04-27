9527aaa05d3d980606aa756d0e66c63a3482b811
Eric Naim <dnaim@cachyos.org>
2025-04-27 19:37:28 +0800

    lsp: Cleanup
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,27 +20,28 @@ return {
                     -- Jump to the definition of the word under your cursor.
                     --  This is where a variable was first declared, or where a function is defined, etc.
                     --  To jump back, press <C-t>.
-                    map('gd', require('snacks').picker.lsp_definitions, '[G]oto [D]efinition')
+                    local snacks_picker = require('snacks').picker
+                    map('gd', snacks_picker.lsp_definitions, '[G]oto [D]efinition')
 
                     -- Find references for the word under your cursor.
-                    map('gr', require('snacks').picker.lsp_references, '[G]oto [R]eferences')
+                    map('gr', snacks_picker.lsp_references, '[G]oto [R]eferences')
 
                     -- Jump to the implementation of the word under your cursor.
                     --  Useful when your language has ways of declaring types without an actual implementation.
-                    map('gI', require('snacks').picker.lsp_implementations, '[G]oto [I]mplementation')
+                    map('gI', snacks_picker.lsp_implementations, '[G]oto [I]mplementation')
 
                     -- Jump to the type of the word under your cursor.
                     --  Useful when you're not sure what type a variable is and you want to see
                     --  the definition of its *type*, not where it was *defined*.
-                    map('gt', require('snacks').picker.lsp_type_definitions, 'Type [D]efinition')
+                    map('gt', snacks_picker.lsp_type_definitions, 'Type [D]efinition')
 
                     -- Fuzzy find all the symbols in your current document.
                     --  Symbols are things like variables, functions, types, etc.
-                    map('gs', require('snacks').picker.lsp_symbols, '[S]ymbols')
+                    map('gs', snacks_picker.lsp_symbols, '[S]ymbols')
 
                     -- Fuzzy find all the symbols in your current workspace.
                     --  Similar to document symbols, except searches over your entire project.
-                    map('gw', require('snacks').picker.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+                    map('gw', snacks_picker.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
 
                     -- Rename the variable under your cursor.
                     --  Most Language Servers support renaming across files, etc.
@@ -54,17 +55,12 @@ return {
                     --  For example, in C this would take you to the header.
                     map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
                     ---@param client vim.lsp.Client
                     ---@param method vim.lsp.protocol.Method
                     ---@param bufnr? integer some lsp support methods only in specific files
                     ---@return boolean
                     local function client_supports_method(client, method, bufnr)
-                        if vim.fn.has 'nvim-0.11' == 1 then
-                            return client:supports_method(method, bufnr)
-                        else
-                            return client.supports_method(method, { bufnr = bufnr })
-                        end
+                        return client:supports_method(method, bufnr)
                     end
 
                     -- The following two autocommands are used to highlight references of the
@@ -158,7 +154,7 @@ return {
                 } or {},
                 virtual_text = {
                     source = 'if_many',
-                    spacing = 4,
+                    spacing = 2,
                     format = function(diagnostic)
                         local diagnostic_message = {
                             [vim.diagnostic.severity.ERROR] = diagnostic.message,
@@ -184,7 +180,6 @@ return {
                             "-j=12", "--header-insertion-decorators", "--pretty",
                             "--enable-config" },
                 },
-                rust_analyzer = {},
                 lua_ls = {
                     settings = {
                         Lua = {
