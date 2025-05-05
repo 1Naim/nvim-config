5ce870c6784f6da8a3baa830d5167eca66f5595e
Eric Naim <dnaim@cachyos.org>
2025-05-06 02:28:07 +0800

    Reapply "init: Use transparent background"
    This reverts commit 59665f3ceeb6db4319288ff312341d330211a8e0.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- init.lua
+++ init.lua
@@ -106,3 +106,7 @@ require 'naim.maps'
 require 'naim.misc'
 
 vim.cmd.colorscheme 'ayu-dark'
+
+-- Set transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
