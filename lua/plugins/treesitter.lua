555db6aa5370a7fcee80a87ced9570c790806a36
Eric Naim <dnaim@cachyos.org>
2025-05-13 16:30:32 +0800

    treesitter: Load early when opening a file from cmdline
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile', 'VeryLazy' },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs',
     opts = {
