bc7821b2da880c345e885472f340bbb78b92508d
Eric Naim <dnaim@cachyos.org>
2025-05-16 02:04:38 +0800

    lsp: Move LspAttach callback into its own function
    This makes it more readable by reducing the indents
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,148 @@
+local function lsp_config(event)
+    local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    -- Jump to the definition of the word under your cursor.
+    --  This is where a variable was first declared, or where a function is defined, etc.
+    --  To jump back, press <C-t>.
+    local snacks_picker = require('snacks').picker
+    map('gd', snacks_picker.lsp_definitions, '[G]oto [D]efinition')
+
+    -- Find references for the word under your cursor.
+    map('gr', snacks_picker.lsp_references, '[G]oto [R]eferences')
+
+    -- Jump to the implementation of the word under your cursor.
+    --  Useful when your language has ways of declaring types without an actual implementation.
+    map('gi', snacks_picker.lsp_implementations, '[G]oto [I]mplementation')
+
+    -- Jump to the type of the word under your cursor.
+    --  Useful when you're not sure what type a variable is and you want to see
+    --  the definition of its *type*, not where it was *defined*.
+    map('gt', snacks_picker.lsp_type_definitions, 'Type [D]efinition')
+
+    -- Fuzzy find all the symbols in your current document.
+    --  Symbols are things like variables, functions, types, etc.
+    map('gs', snacks_picker.lsp_symbols, '[S]ymbols')
+
+    -- Fuzzy find all the symbols in your current workspace.
+    --  Similar to document symbols, except searches over your entire project.
+    map('gw', snacks_picker.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- Rename the variable under your cursor.
+    --  Most Language Servers support renaming across files, etc.
+    map('gn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+    -- Execute a code action, usually your cursor needs to be on top of an error
+    -- or a suggestion from your LSP for this to activate.
+    map('ga', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+    -- WARN: This is not Goto Definition, this is Goto Declaration.
+    --  For example, in C this would take you to the header.
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+    -- Show signature
+    map('<C-k>', vim.lsp.buf.signature_help, 'Show signature', 'i')
+
+    ---@param client vim.lsp.Client
+    ---@param method vim.lsp.protocol.Method
+    ---@param bufnr? integer some lsp support methods only in specific files
+    ---@return boolean
+    local function client_supports_method(client, method, bufnr)
+        return client:supports_method(method, bufnr)
+    end
+
+    -- The following two autocommands are used to highlight references of the
+    -- word under your cursor when your cursor rests there for a little while.
+    --    See `:help CursorHold` for information about when this is executed
+    --
+    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client then
+        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                end,
+            })
+        end
+
+        -- Toggle inlay hints
+        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+            map('<leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+        end
+
+        -- Native Autocomplete
+        if client_supports_method(client, vim.lsp.protocol.textDocument_completion, event.buf) then
+            vim.opt.shm:append 'c'
+            -- Trigger autocomplete on every keystroke
+            local chars = {}
+            for i = 32, 126 do
+                table.insert(chars, string.char(i))
+            end
+            client.server_capabilities.completionProvider.triggerCharacters = chars
+
+            require('mini.icons').tweak_lsp_kind()
+            vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+
+            -- Manually trigger completeion
+            map('<C-space>', vim.lsp.completion.get, 'Trigger Autocomplete', 'i')
+
+            -- Tab to select next item or insert tab
+            vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+                if vim.fn.pumvisible() == 1 then
+                    return '<C-n>'
+                elseif vim.snippet.active { direction = 1 } then
+                    return '<Cmd>lua vim.snippet.jump(1)<CR>'
+                else
+                    return '<Tab>'
+                end
+            end, { expr = true, noremap = true })
+
+            -- Shift-Tab to select previous item
+            vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+                if vim.fn.pumvisible() == 1 then
+                    return '<C-p>'
+                elseif vim.snippet.active { direction = -1 } then
+                    return '<Cmd>lua vim.snippet.jump(-1)<CR>'
+                else
+                    return '<S-Tab>'
+                end
+            end, { expr = true, noremap = true })
+
+            -- Enter to confirm selection (or just insert newline)
+            vim.keymap.set('i', '<CR>', function()
+                if vim.fn.pumvisible() == 1 then
+                    return '<C-y>'
+                else
+                    return '<CR>'
+                end
+            end, { expr = true, noremap = true })
+
+            vim.keymap.set('s', 'BS', '<C-o>s', {})
+        end
+    end
+end
+
 return {
     {
         'neovim/nvim-lspconfig',
@@ -18,150 +163,7 @@ return {
         config = function()
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-                callback = function(event)
-                    local map = function(keys, func, desc, mode)
-                        mode = mode or 'n'
-                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-                    end
-
-                    -- Jump to the definition of the word under your cursor.
-                    --  This is where a variable was first declared, or where a function is defined, etc.
-                    --  To jump back, press <C-t>.
-                    local snacks_picker = require('snacks').picker
-                    map('gd', snacks_picker.lsp_definitions, '[G]oto [D]efinition')
-
-                    -- Find references for the word under your cursor.
-                    map('gr', snacks_picker.lsp_references, '[G]oto [R]eferences')
-
-                    -- Jump to the implementation of the word under your cursor.
-                    --  Useful when your language has ways of declaring types without an actual implementation.
-                    map('gi', snacks_picker.lsp_implementations, '[G]oto [I]mplementation')
-
-                    -- Jump to the type of the word under your cursor.
-                    --  Useful when you're not sure what type a variable is and you want to see
-                    --  the definition of its *type*, not where it was *defined*.
-                    map('gt', snacks_picker.lsp_type_definitions, 'Type [D]efinition')
-
-                    -- Fuzzy find all the symbols in your current document.
-                    --  Symbols are things like variables, functions, types, etc.
-                    map('gs', snacks_picker.lsp_symbols, '[S]ymbols')
-
-                    -- Fuzzy find all the symbols in your current workspace.
-                    --  Similar to document symbols, except searches over your entire project.
-                    map('gw', snacks_picker.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
-
-                    -- Rename the variable under your cursor.
-                    --  Most Language Servers support renaming across files, etc.
-                    map('gn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-                    -- Execute a code action, usually your cursor needs to be on top of an error
-                    -- or a suggestion from your LSP for this to activate.
-                    map('ga', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-                    -- WARN: This is not Goto Definition, this is Goto Declaration.
-                    --  For example, in C this would take you to the header.
-                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-                    -- Show signature
-                    map('<C-k>', vim.lsp.buf.signature_help, 'Show signature', 'i')
-
-                    ---@param client vim.lsp.Client
-                    ---@param method vim.lsp.protocol.Method
-                    ---@param bufnr? integer some lsp support methods only in specific files
-                    ---@return boolean
-                    local function client_supports_method(client, method, bufnr)
-                        return client:supports_method(method, bufnr)
-                    end
-
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client then
-                        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-                            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-                            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                                buffer = event.buf,
-                                group = highlight_augroup,
-                                callback = vim.lsp.buf.document_highlight,
-                            })
-
-                            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                                buffer = event.buf,
-                                group = highlight_augroup,
-                                callback = vim.lsp.buf.clear_references,
-                            })
-
-                            vim.api.nvim_create_autocmd('LspDetach', {
-                                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-                                callback = function(event2)
-                                    vim.lsp.buf.clear_references()
-                                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-                                end,
-                            })
-                        end
-
-                        -- Toggle inlay hints
-                        if client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-                            vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
-                            map('<leader>th', function()
-                                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-                            end, '[T]oggle Inlay [H]ints')
-                        end
-
-                        -- Native Autocomplete
-                        if client_supports_method(client, vim.lsp.protocol.textDocument_completion, event.buf) then
-                            vim.opt.shm:append 'c'
-                            -- Trigger autocomplete on every keystroke
-                            local chars = {}
-                            for i = 32, 126 do
-                                table.insert(chars, string.char(i))
-                            end
-                            client.server_capabilities.completionProvider.triggerCharacters = chars
-
-                            require('mini.icons').tweak_lsp_kind()
-                            vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-
-                            -- Manually trigger completeion
-                            map('<C-space>', vim.lsp.completion.get, 'Trigger Autocomplete', 'i')
-
-                            -- Tab to select next item or insert tab
-                            vim.keymap.set({ 'i', 's' }, '<Tab>', function()
-                                if vim.fn.pumvisible() == 1 then
-                                    return '<C-n>'
-                                elseif vim.snippet.active { direction = 1 } then
-                                    return '<Cmd>lua vim.snippet.jump(1)<CR>'
-                                else
-                                    return '<Tab>'
-                                end
-                            end, { expr = true, noremap = true })
-
-                            -- Shift-Tab to select previous item
-                            vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
-                                if vim.fn.pumvisible() == 1 then
-                                    return '<C-p>'
-                                elseif vim.snippet.active { direction = -1 } then
-                                    return '<Cmd>lua vim.snippet.jump(-1)<CR>'
-                                else
-                                    return '<S-Tab>'
-                                end
-                            end, { expr = true, noremap = true })
-
-                            -- Enter to confirm selection (or just insert newline)
-                            vim.keymap.set('i', '<CR>', function()
-                                if vim.fn.pumvisible() == 1 then
-                                    return '<C-y>'
-                                else
-                                    return '<CR>'
-                                end
-                            end, { expr = true, noremap = true })
-
-                            vim.keymap.set('s', 'BS', '<C-o>s', {})
-                        end
-                    end
-                end,
+                callback = lsp_config
             })
 
             ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
