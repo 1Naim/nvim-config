7a0d00ca60d33f6256a91c5c2cd13262ff7942fa
Eric Naim <dnaim@cachyos.org>
2025-11-13 09:56:58 +0800

    treesitter: Only install necessary languages and rework config
    There really is no need to install *all* parsers for any file that I've
    opened. This is just bloat and completely unnecessary/unused since some
    parsers are just one-off files that I'll be opening once a blue moon.
    Therefore, rework the config to only install the parsers that I'll know
    I use. Configuration is based on https://github.com/MeanderingProgrammer/treesitter-modules.nvim?tab=readme-ov-file#implementing-yourself
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,49 +1,9 @@
-local function install_and_start()
-    -- Auto-install and start treesitter parser for any buffer with a registered filetype
-    vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-        callback = function(event)
-            local bufnr = event.buf
-            local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
-
-            -- Skip if no filetype
-            if filetype == '' then
-                return
-            end
-
-            -- Get parser name based on filetype
-            local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
-            if not parser_name then
-                return
-            end
-            -- Try to get existing parser (helpful check if filetype was returned above)
-            local parser_configs = require 'nvim-treesitter.parsers'
-            if not parser_configs[parser_name] then
-                return -- Parser not available, skip silently
-            end
-
-            local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-            if not parser_installed then
-                -- If not installed, install parser synchronously
-                require('nvim-treesitter').install({ parser_name }):wait(30000)
-            end
-
-            -- let's check again
-            parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-            if parser_installed then
-                -- Start treesitter for this buffer
-                local max_filesize = 1024 * 1024 * 25 -- 25MB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-                if ok and stats and stats.size > max_filesize then
-                    vim.notify('Disabling treesitter because filesize is too large', vim.log.levels.WARN, { title = 'Treesitter' })
-                    return true
-                end
-                vim.treesitter.start(bufnr, parser_name)
-            end
-        end,
-    })
-end
+local languages = {
+    'bash', 'fish',
+    'c', 'cpp', 'kconfig', 'make',
+    'gitcommit', 'html', 'markdown', 'markdown_inline',
+    'lua', 'java', 'python', 'r', 'rust'
+}
 
 return {
     {
@@ -53,9 +13,35 @@ return {
         lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
         build = ':TSUpdate',
         config = function()
+            require('nvim-treesitter').install(languages)
             vim.treesitter.language.register('bash', 'PKGBUILD')
-            vim.treesitter.language.register('diff', 'gitsendemail')
-            install_and_start()
+
+            vim.api.nvim_create_autocmd('FileType', {
+                group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+                callback = function(args)
+                    local buf = args.buf
+                    local filetype = args.match
+
+                    -- you need some mechanism to avoid running on buffers that do not
+                    -- correspond to a language (like oil.nvim buffers), this implementation
+                    -- checks if a parser exists for the current language
+                    local language = vim.treesitter.language.get_lang(filetype) or filetype
+                    if not vim.treesitter.language.add(language) then
+                        return
+                    end
+
+                    -- replicate `fold = { enable = true }`
+                    -- vim.wo.foldmethod = 'expr'
+                    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+                    -- replicate `highlight = { enable = true }`
+                    vim.treesitter.start(buf, language)
+
+                    -- replicate `indent = { enable = true }`
+                    -- vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+
         end,
     },
     {
