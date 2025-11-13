0b0ee3f2771558ddb8632dc78dc8dbdc661df180
Eric Naim <dnaim@cachyos.org>
2025-11-13 13:37:41 +0800

    treesitter: Refactor callback function for autocmd
    Move it into its own local function and call that in the callback event
    for less indent nesting. Also add back functionality to not enable
    treesitter when the file is too huge.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,33 @@
-local languages = {
-    'bash', 'fish',
-    'c', 'cpp', 'kconfig', 'make',
-    'gitcommit', 'html', 'markdown', 'markdown_inline',
-    'lua', 'java', 'python', 'r', 'rust'
-}
+local function enable_treesitter(args)
+    local buf = args.buf
+    local filetype = args.match
+
+    -- you need some mechanism to avoid running on buffers that do not
+    -- correspond to a language (like oil.nvim buffers), this implementation
+    -- checks if a parser exists for the current language
+    local language = vim.treesitter.language.get_lang(filetype) or filetype
+    if not vim.treesitter.language.add(language) then
+        return
+    end
+
+    -- Disable for files that are too big
+    local max_filesize = 1024 * 1024 * 25 -- 25MB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+        vim.notify('Disabling treesitter because filesize is too large', vim.log.levels.WARN, { title = 'Treesitter' })
+        return true
+    end
+
+    -- replicate `fold = { enable = true }`
+    -- vim.wo.foldmethod = 'expr'
+    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+    -- replicate `highlight = { enable = true }`
+    vim.treesitter.start(buf, language)
+
+    -- replicate `indent = { enable = true }`
+    -- vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
 
 return {
     {
@@ -13,35 +37,20 @@ return {
         lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
         build = ':TSUpdate',
         config = function()
+            local languages = {
+                'bash', 'fish',
+                'c', 'cpp', 'kconfig', 'make',
+                'gitcommit', 'html', 'markdown', 'markdown_inline',
+                'lua', 'java', 'python', 'r', 'rust'
+            }
+
             require('nvim-treesitter').install(languages)
             vim.treesitter.language.register('bash', 'PKGBUILD')
 
             vim.api.nvim_create_autocmd('FileType', {
                 group = vim.api.nvim_create_augroup('treesitter.setup', {}),
-                callback = function(args)
-                    local buf = args.buf
-                    local filetype = args.match
-
-                    -- you need some mechanism to avoid running on buffers that do not
-                    -- correspond to a language (like oil.nvim buffers), this implementation
-                    -- checks if a parser exists for the current language
-                    local language = vim.treesitter.language.get_lang(filetype) or filetype
-                    if not vim.treesitter.language.add(language) then
-                        return
-                    end
-
-                    -- replicate `fold = { enable = true }`
-                    -- vim.wo.foldmethod = 'expr'
-                    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
-                    -- replicate `highlight = { enable = true }`
-                    vim.treesitter.start(buf, language)
-
-                    -- replicate `indent = { enable = true }`
-                    vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                end,
+                callback = enable_treesitter,
             })
-
         end,
     },
     {
