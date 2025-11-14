090f843d317c759ba4594ad6e5ee0548e3bc309b
Eric Naim <dnaim@cachyos.org>
2025-11-14 15:59:28 +0800

    guess-indent: Make sure tab characters are always 8 columns
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/guess-indent.lua
+++ lua/plugins/guess-indent.lua
@@ -1,5 +1,12 @@
 return {
     'NMAC427/guess-indent.nvim',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-    opts = {},
+    opts = {
+        on_space_options = { -- A table of vim options when spaces are detected
+            ["expandtab"] = true,
+            ["tabstop"] = 8, -- Make sure tab characters are always 8 columns
+            ["softtabstop"] = "detected",
+            ["shiftwidth"] = "detected",
+        },
+    },
 }
