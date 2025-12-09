1d2cdc33b5138c5cf0c34f96541eeb037de0cf5e
Eric Naim <dnaim@cachyos.org>
2025-12-09 23:45:37 +0800

    colors: ayu: Do not make the cursor line transparent
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,34 +1,36 @@
 return {
-    'Shatur/neovim-ayu',
-    priority = 1000,
-    lazy = false,
-    config = function()
-        local colors = require 'ayu.colors'
-        colors.generate() -- Pass `true` to enable mirage
+    {
+        'Shatur/neovim-ayu',
+        priority = 1000,
+        -- enabled = false,
+        lazy = false,
+        config = function()
+            local colors = require 'ayu.colors'
+            colors.generate() -- Pass `true` to enable mirage
 
-        require('ayu').setup {
-            overrides = function()
-                return {
-                    Comment = { fg = colors.comment, italic = false },
-                    LineNr = { fg = colors.comment },
-                    BlinkCmpMenuSelection = {
-                        fg = colors.accent,
-                        reverse = true,
-                    },
-                    NonText = { fg = '#3C424E' },
-                    --[[ Transparency
-                    Normal = { bg = 'None' },
-                    NormalFloat = { bg = 'none' },
-                    SignColumn = { bg = 'None' },
-                    Folded = { bg = 'None' },
-                    FoldColumn = { bg = 'None' },
-                    CursorLine = { bg = 'None' },
-                    CursorColumn = { bg = 'None' },
-                    VertSplit = { bg = 'None' },
-                    ]]
-                }
-            end,
-        }
-        vim.cmd.colorscheme 'ayu-dark'
-    end,
+            require('ayu').setup {
+                overrides = function()
+                    return {
+                        Comment = { fg = colors.comment, italic = false },
+                        LineNr = { fg = colors.comment },
+                        BlinkCmpMenuSelection = {
+                            fg = colors.accent,
+                            reverse = true,
+                        },
+                        NonText = { fg = '#3C424E' },
+                        -- Transparency
+                        Normal = { bg = 'None' },
+                        NormalFloat = { bg = 'none' },
+                        SignColumn = { bg = 'None' },
+                        Folded = { bg = 'None' },
+                        FoldColumn = { bg = 'None' },
+                        -- CursorLine = { bg = 'None' },
+                        -- CursorColumn = { bg = 'None' },
+                        VertSplit = { bg = 'None' },
+                    }
+                end,
+            }
+            vim.cmd.colorscheme 'ayu-dark'
+        end,
+    },
 }
