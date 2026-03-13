bdc6bbf9b3634d189a1e8ecdde4d9fcc36bf956a
Eric Naim <dnaim@cachyos.org>
2026-03-14 02:55:07 +0800

    treesitter: Use build in vim.pack.add spec instead of PackChange aucmd
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- plugin/30-treesitter.lua
+++ plugin/30-treesitter.lua
@@ -18,7 +18,10 @@ local languages = {
 
 MINI_NOW_IF_ARGS(function()
     PACK_ADD {
-        'https://github.com/nvim-treesitter/nvim-treesitter',
+        {
+            src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+            build = ':TSUpdate',
+        },
         'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
     }
 
@@ -61,26 +64,6 @@ MINI_NOW_IF_ARGS(function()
             -- replicate `indent = { enable = true }`
             -- still broken
             -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-    })
-
-    vim.api.nvim_create_autocmd('PackChanged', {
-        desc = 'Handle nvim-treesitter updates',
-        group = vim.api.nvim_create_augroup(
-            'nvim-treesitter-pack-changed-update-handler',
-            { clear = true }
-        ),
-        callback = function(event)
-            if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
-                vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
-                ---@diagnostic disable-next-line: param-type-mismatch
-                local ok = pcall(vim.cmd, 'TSUpdate')
-                if ok then
-                    vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
-                else
-                    vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
-                end
-            end
         end,
     })
 end)
