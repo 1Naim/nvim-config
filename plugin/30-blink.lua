0d1f45c65b06d0603873e203cc7a105a53ffa6ae
Eric Naim <dnaim@cachyos.org>
2026-03-16 18:50:59 +0800

    blink: Disable on Linux kernel repository
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- plugin/30-blink.lua
+++ plugin/30-blink.lua
@@ -48,7 +48,7 @@ MINI_NOW_IF_ARGS(function()
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
-            default = { 'git', 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'git', 'lsp', 'path', 'buffer' },
             providers = {
                 git = {
                     module = 'blink-cmp-git',
@@ -61,6 +61,15 @@ MINI_NOW_IF_ARGS(function()
                     end,
                     opts = {
                         commit = {
+                            enable = function()
+                                if vim.fn.getcwd(0, 0) == '/mnt/Media/CachyOS-Git/linux/main' then
+                                    return false
+                                end
+
+                                local utils = require('blink-cmp-git.utils')
+                                return utils.is_inside_git_repo()
+                            end,
+
                             -- Insert first 12 hash and commit title
                             get_insert_text = function(item)
                                 return item:match('commit ([^\n]*)'):sub(1, 12)
