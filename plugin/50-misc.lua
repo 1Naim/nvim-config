58c99c71e872412b9577897cb0cae64388e4cae3
Eric Naim <dnaim@cachyos.org>
2026-03-16 22:27:21 +0800

    misc: Delete trailing whitespaces on write
    This is more useful than I thought
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- plugin/50-misc.lua
+++ plugin/50-misc.lua
@@ -21,3 +21,21 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
         require('mini.misc').setup_auto_root()
     end,
 })
+
+-- Delete trailing whitespaces on write
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    pattern = { '*' },
+    callback = function()
+        local blacklist = {
+            ['gitsendemail'] = true,
+            ['diff'] = true,
+            ['gitcommit'] = true,
+        }
+        local ft = vim.bo.filetype
+        if blacklist[ft] then return end
+
+        local save_cursor = vim.fn.getpos '.'
+        pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+        vim.fn.setpos('.', save_cursor)
+    end,
+})
