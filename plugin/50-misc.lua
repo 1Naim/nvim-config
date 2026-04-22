e66b6be487ed0d6dae7a3488cac356ac773adf44
Eric Naim <dnaim@cachyos.org>
2026-04-22 14:17:51 +0800

    misc: Add lazy load wrappers for aucmd
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- plugin/50-misc.lua
+++ plugin/50-misc.lua
@@ -2,49 +2,59 @@ MINI_NOW_IF_ARGS(function() PACK_ADD { 'https://github.com/lambdalisue/vim-suda'
 
 MINI_LATER(function() vim.cmd 'packadd nvim.undotree' end)
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    callback = function() vim.hl.on_yank() end,
-})
+MINI_LATER(function()
+    vim.api.nvim_create_autocmd('TextYankPost', {
+        desc = 'Highlight when yanking (copying) text',
+        callback = function() vim.hl.on_yank() end,
+    })
+end)
 
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
-    desc = 'Automatically set the root directory',
-    callback = function()
-        local kernel_root = '/mnt/Media/CachyOS-Git/linux/main'
-        local file = vim.api.nvim_buf_get_name(0)
+MINI_NOW(function()
+    vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
+        desc = 'Automatically set the root directory',
+        callback = function()
+            local kernel_root = '/mnt/Media/CachyOS-Git/linux/main'
+            local file = vim.api.nvim_buf_get_name(0)
 
-        if vim.startswith(file, kernel_root) then
-            vim.cmd('lcd ' .. vim.fn.fnameescape(kernel_root))
-            return
-        end
+            if vim.startswith(file, kernel_root) then
+                vim.cmd('lcd ' .. vim.fn.fnameescape(kernel_root))
+                return
+            end
 
-        require('mini.misc').setup_auto_root()
-    end,
-})
+            require('mini.misc').setup_auto_root()
+        end,
+    })
+end)
 
 -- Delete trailing whitespaces on write
-vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-    pattern = { '*' },
-    callback = function()
-        local blacklist = {
-            ['gitsendemail'] = true,
-            ['diff'] = true,
-            ['gitcommit'] = true,
+MINI_LATER(function()
+    vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+        pattern = { '*' },
+        callback = function()
+            local blacklist = {
+                ['gitsendemail'] = true,
+                ['diff'] = true,
+                ['gitcommit'] = true,
+            }
+            local ft = vim.bo.filetype
+            if blacklist[ft] then return end
+
+            local save_cursor = vim.fn.getpos '.'
+            pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+            vim.fn.setpos('.', save_cursor)
+        end,
+    })
+end)
+
+MINI_NOW(
+    function()
+        vim.filetype.add {
+            pattern = {
+                ['.*git/config'] = 'gitconfig',
+            },
+            filename = {
+                ['PKGBUILD'] = 'sh',
+            },
         }
-        local ft = vim.bo.filetype
-        if blacklist[ft] then return end
-
-        local save_cursor = vim.fn.getpos '.'
-        pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-        vim.fn.setpos('.', save_cursor)
-    end,
-})
-
-vim.filetype.add {
-    pattern = {
-        ['.*git/config'] = 'gitconfig',
-    },
-    filename = {
-        ['PKGBUILD'] = 'sh',
-    },
-}
+    end
+)
